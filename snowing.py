c9b3d29f9f4bbf45208d23e4975d9744bded4284
Kandhy0X9 <kashengkandhy@gmail.com>
2026-02-12 15:16:02 -0500

    make the snow flakes appear on screen

--- snowing.py
+++ snowing.py
@@ -9,8 +9,7 @@ speed(0)
 pensize(4)
 color("white")
 
-# SnowFlake
-
+# SnowFlake functions
 def diagnolLine(size):
     # make first diagnol line
     forward(size*2) # makes twice the normal length
@@ -20,29 +19,31 @@ def diagnolLine(size):
 
 def snowFlake(size):
     forward(size)
-    backward(size*2)
+    backward(size*2) # moves back to make the whole line
     forward(size)
     right(45)
-    diagnolLine(size)
+    diagnolLine(size) # make the first diagnol line
     # make the small vertical line
     forward(size/2) # makes half of the normal length
-    backward(size)
+    backward(size) # moves back to make the whole line
     forward(size/2)
     right(45)
-    diagnolLine(size)
+    diagnolLine(size) # make the second diagnol line
 
-i = 0
-while i <=50:
-    size =  random.randint(5,20)
-    # goto to random position
-    posX = random.randint(-1500,1500) # go to random position on x
-    posY = random.randint(-1001,1001) # go to random position on y
+def snowPosition():
+    posX = window_width() / 2 # get the width of the window and divide by 2 to get the range for x coordinates
+    posY = window_height() / 2 # get the height of the window and divide by 2 to get the range for y coordinates
     penup()
-    goto(posX,posY)
-    pendown()
-    snowFlake(size)
-    i+=1
+    goto(random.randint(-posX, posX), random.randint(-posY, posY)) # randomly position the turtle within the window
+    pendown() 
 
+# draw 50 snowflakes
+for i in range(50):
+    # randomly position the turtle and draw a snowflake
+    snowPosition()
+    setheading(0) # resets turtle's direction to the right
+    size = random.randint(5, 15) # random size for snowflakes
+    snowFlake(size)
 
 # keep the window open
 done()
